a1a29d5ccc11d5e2849ad42ff4623e84a04e1e39
cyries012 <soluchkonrad@gmail.com>
2018-03-12 20:04:53 +0100

    commit

--- js/js.js
+++ js/js.js
@@ -1,12 +1,40 @@
+var counter = 6;
 var ulList = document.querySelector('.ulList');
 var addButton = document.querySelector('.addButton'); 
 var deleteButton = document.querySelector('.deleteButton'); 
 var ourHeadline = document.querySelector('.headline');
-    
-        
+var listToDo = document.querySelector('.listToDo');
+var listToDone = document.querySelector('.listToDone');
+ 
+var checkedTwo = document.querySelectorAll('input');  
+   /*     
+function checked(e) {
+    listToDo.innerHTML = '';
+    var checkedOne = document.querySelectorAll('.one'); 
+for (let j = 0; j < checkedOne.length; j++)
+    { 
+        if (checkedOne[j].checked = true)
+            {console.log(checkedOne[j].parentElement);
+                listToDo.innerHTML += `<li> ${checkedOne[j].parentElement.textContent}  </li>`;
+            }     
+    }
+}
+*/
+ulList.addEventListener("click", nono)
+function nono(e) {
+console.log(e.path[0].childNodes[1].checked);
+//if (checkedTwo[e].checked == true)
+//{
+  //  listToDo.innerHTML += `<li> ${checkedTwo[0].parentElement.textContent}  </li>`;
+//}
+}
+
+
+
+
 function activateItem(e){
 if (e.target.nodeName == "LI") {
-        ourHeadline.innerHTML = e.target.innerHTML; 
+        ourHeadline.innerHTML = e.target.textContent; 
  
         for (let i = 0; i < e.target.parentNode.children.length; i++)
         {
@@ -19,7 +47,8 @@ if (e.target.nodeName == "LI") {
 
 function addItem() {
     let textLi = prompt("Enter name new item") || "undefinited";
-    ulList.innerHTML += `<li> ${textLi}</li>`;
+    ulList.innerHTML += `<li> ${textLi}<input class="one" checked name="${counter}" type="radio"><input class="two" name="${counter}" type="radio"></li>`;
+    counter++;
 }
 
 
@@ -36,3 +65,4 @@ function deleteItem(e) {
 ulList.addEventListener("click", activateItem);
 addButton.addEventListener("click", addItem);
 deleteButton.addEventListener("click", deleteItem);
+
